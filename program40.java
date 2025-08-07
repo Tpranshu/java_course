316a0a8c35248f665031ddfb6916fb6300559bae
Tpranshu <tpranshu118@gmail.com>
2025-08-07 07:03:33 -0700

    sample commit

---
+++ program40.java
@@ -0,0 +1,26 @@
+public class program40 {
+    public static void main(String[] args) {
+        // Interpreted vs Compiled Languages
+        /*
+
+         *  | Feature        | Interpreter          | Compiler                 |
+            | -------------- | -------------------- | ------------------------ |
+            | How it works   | Line by line         | Whole program at once    |
+            | Speed          | Slower               | Faster after compiling   |
+            | Error checking | Stops at first error | Shows all errors at once |
+            | Output         | Runs directly        | Creates a separate file  |
+
+        Interpreter --
+        An interpreter reads and runs your code line by line.
+        If there’s a mistake, it stops right away and shows the error.
+
+        Example: Python, JavaScript
+
+        Compiler --
+        A compiler reads the entire code at once, checks everything, and then turns it into a separate file (machine code) that the computer can run.
+
+        Example: C, C++
+        
+         */
+    }
+}
---
+++ program41.java
@@ -0,0 +1,16 @@
+public class program41 {
+    public static void main(String[] args) {
+        
+        /*
+         * Java is both compiled and interpreted.
+         * 
+         * Java code is compiled into bytecode using the Java compiler.
+         * 
+         * The Java Virtual Machine (JVM) then interprets or JIT-compiles the bytecode
+         * into machine code.
+         * 
+         * This makes Java platform-independent — it can run on any system with a J
+         * 
+         */
+    }
+}
